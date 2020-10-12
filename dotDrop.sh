ea29fea7f8c9edd5c62a4f29ba8b7def9a3c2267
MrMip <Miphamster@protonmail.com>
2020-10-12 11:23:57 -0500

    init

---
+++ dotDrop.sh
@@ -0,0 +1,25 @@
+#!/bin/bash 
+
+#Settings
+AURHELPER="yay"
+GITUSERNAME="MrMip"
+REPOS=(dotfiles dwm)
+
+echo "Installing Packages"
+sudo pacman -S --needed - < pagklist
+
+echo "Installing AUR Helper"
+git clone https://aur.archlinux.org/$AURHELPER.git
+cd yay
+makepkg -si
+cd .. && rm -rf yay
+echo "Installing AUR Packages"
+sudo yay -S --needed - < aurpagklist
+
+echo "Cloneing Repo(s)"
+i=0
+while [ $i <= ${REPOS[@]} ]; do
+    git clone https://github.com/$GITUSERNAME/${REPOS[$i]}.git
+    $i=$(expr $i + 1)  
+done
+
